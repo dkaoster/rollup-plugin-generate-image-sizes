61623a24acc090339926ddc9b19cb82bf62cc2e3
Daniel Kao <dkao@diplateevo.com>
2020-11-27 00:05:57 +0800

    Improve Options and Add Lint

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,3 @@
 export default {
   input: 'src/index.js',
-}
+};
--- src/index.js
+++ src/index.js
@@ -5,45 +5,72 @@ import sharp from 'sharp';
 /**
  * Generate different image sizes on all images found within a directory,
  * based on the configuration given.
- *
- * @param options
- * @returns {{buildEnd(): void, name: string}}
  */
 
-export default (options) => ({
-  name: 'generate-image-sizes',
-  // Runs on renderStart hook.
-  // First, get all the image files in the given directory
-  renderStart: () => globby(`${options.dir}/**/!(*@*|*#*).{jpg,jpeg,png}`)
-    .then((images) => Promise.allSettled(
-      // Map them into sharp objects
-      images.map((image) => {
-        const sharpObj = sharp(image);
+export default (options = {}) => {
+  // Load options
+  const {
+    hook = 'renderStart',
+    quality = 65,
+    dir = '.',
+    inputFormats = ['jpg', 'jpeg', 'png'],
+    outputFormats = ['jpg'],
+    sizes = [1400, 1024, 640, 320],
+  } = options;
 
-        // Read the sharp metadata because we don't want to do stupid
-        // things like scale up (pointless)
-        return sharpObj.metadata()
-          .then((metadata) => Promise.allSettled(
-            options.sizes.map((scaleWidth) => {
-              // generate the output path
-              const imagePathSplit = image.split('.');
-              const imagePathPre = imagePathSplit.slice(0, -1).join('.');
-              const imagePathPost = imagePathSplit[imagePathSplit.length - 1];
-              const fileOut = `${imagePathPre}@${scaleWidth}w.${imagePathPost}`;
+  return {
+    name: 'generate-image-sizes',
+    // Runs on the hook specified, otherwise the renderStart hook.
+    // First, get all the image files in the given directory
+    [hook]: () => {
+      if (!inputFormats) return Promise.resolve();
+      return globby(`${dir}/**/!(*@*|*#*).{${inputFormats.join(',')}`)
+        .then((images) => Promise.allSettled(
+        // Map them into sharp objects
+          images.map((image) => {
+            const sharpObj = sharp(image);
 
-              // If the width we want to scale to is larger than the original
-              // width, we return a relative symlink to the original file.
-              if (scaleWidth > metadata.width) {
-                const imagePath = image.split('/');
-                const relativePath = `./${imagePath[imagePath.length - 1]}`;
-                // TODO windows can't symlink
-                return fs.symlink(relativePath, fileOut);
-              }
+            // generate the output path
+            const imagePathSplit = image.split('.');
+            const imagePathPre = imagePathSplit.slice(0, -1).join('.');
+            const imagePathPost = imagePathSplit[imagePathSplit.length - 1];
 
-              // Output this image
-              return sharpObj.resize(scaleWidth).toFile(fileOut);
-            }).filter((p) => !!p),
-          ));
-      }),
-    )),
-});
+            // Save a jpg copy of this image if we don't have one already
+            if (imagePathPost !== 'jpg') {
+              sharpObj.jpeg({ quality }).toFile(`${imagePathPre}.jpg`);
+            }
+
+            // Read the sharp metadata because we don't want to do stupid
+            // things like scale up (pointless)
+            return sharpObj.metadata()
+              .then((metadata) => Promise.allSettled(
+                sizes.map((scaleWidth) => {
+                  const jpgOut = `${imagePathPre}@${scaleWidth}w.jpg`;
+                  const webpOut = `${imagePathPre}@${scaleWidth}w.webp`;
+
+                  // Output webp
+                  sharpObj
+                    .resize(scaleWidth)
+                    .webp({ quality })
+                    .toFile(webpOut);
+
+                  // If the width we want to scale to is larger than the original
+                  // width, we return a relative symlink to the original file.
+                  if (scaleWidth > metadata.width && imagePathPost === 'jpg') {
+                    const imagePath = image.split('/');
+                    const relativePath = `./${imagePath[imagePath.length - 1]}`;
+                    return fs.symlink(relativePath, jpgOut);
+                  }
+
+                  // Output this image
+                  return sharpObj
+                    .resize(scaleWidth)
+                    .jpeg({ quality })
+                    .toFile(jpgOut);
+                }).filter((p) => !!p),
+              ));
+          }),
+        ));
+    },
+  };
+};
